0e573e14f6ee3acf690d3adf0fbce05fb17f4c75
azooz59T <mohamedoo1997@hotmail.com>
2025-10-25 17:36:31 +0300

    Intial commit for setting up Spring Boot

---
+++ src/main/java/com/example/textcomparer/TextComparerApplication.java
@@ -0,0 +1,13 @@
+package com.example.textcomparer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextComparerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextComparerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/textcomparer/TextComparerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.textcomparer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextComparerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
